ffc767158c3d557d1fd71d763bf12d61f99231e1
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-07 15:58:22 +0700

    Praktikum 2 - Soal No. 7 _ About

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return "Nama: Adinda Mirza Devani <br> NIM: 2341720046";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
@@ -13,7 +14,7 @@ Route::get('/world', function () {
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/about', [PageController::class, 'about']);
+Route::get('/about', [AboutController::class, 'about']);
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama Saya '.$name;
