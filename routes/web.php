d491a63b0ddd0f3f05dce34b64ae0e871330bb12
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-07 15:44:52 +0700

    Praktikum 2 - Soal No. 7 _ Home

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
@@ -10,7 +11,7 @@ Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/about', [PageController::class, 'about']);
 
