d7cb3184e9eb9ee43efb784eff4af9db995cebe0
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-07 17:22:22 +0700

    Praktikum 3 - Soal No. 8-10

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,4 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
-Route::get('/greeting', function() {
-    return view('blog.hello', ['name' => 'Funi']);
-});
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
