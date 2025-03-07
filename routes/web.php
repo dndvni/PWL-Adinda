f2162930e2dafba35322c344acadf9e9accba631
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-07 08:39:31 +0700

    Praktikum 2 - Soal No. 2, 3, 4

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return 'Hello World';
-
-});
+Route::get('hello', [WelcomeController::class,'hello']);
 
 Route::get('/world', function () {
     return 'World';
